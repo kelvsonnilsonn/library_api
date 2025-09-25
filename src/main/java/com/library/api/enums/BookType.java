3523090658abed5f9652ed8e8b6207a0530b502f
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-25 15:47:48 -0300

    feat: add common book types for characterize books.

--- src/main/java/com/library/api/enums/BookType.java
+++ src/main/java/com/library/api/enums/BookType.java
@@ -1,4 +1,20 @@
 package com.library.api.enums;
 
 public enum BookType {
+    ROMANCE,
+    TERROR,
+    DRAMA,
+    SUSPENSE,
+    CIENTIFICO,
+    FICCAO_CIENTIFICA,
+    FANTASIA,
+    HISTORIA,
+    BIOGRAFIA,
+    AUTOAJUDA,
+    POESIA,
+    INFANTIL,
+    JUVENIL,
+    CULINARIA,
+    SAUDE,
+    VIAGENS
 }
