4f8bc1494fa9a050c60e125ca131096caebbc7b0
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-25 16:00:09 -0300

    feat: add Password class which contains password attribute and validate his self.
    - add @Embeddable annotation to embed into an entity.

--- src/main/java/com/library/api/model/Password.java
+++ src/main/java/com/library/api/model/Password.java
@@ -1,8 +1,27 @@
 package com.library.api.model;
 
+import com.library.api.exception.InvalidCreatePasswordException;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Password {
     public String password;
+
+    public Password(String password){
+        validate(password);
+        this.password = password;
+    }
+
+    public Password of(String oPassword){
+        return new Password(oPassword);
+    }
+
+    private void validate(String pass){
+        if(pass == null || pass.length() < 3){
+            throw new InvalidCreatePasswordException();
+        }
+    }
 }
