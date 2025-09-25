028b84fd00bdc82127924bb0aac76747fe2affe1
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-25 15:45:15 -0300

    feat(service): add book creation logic
    - Adds a new 'create' method to save a book to the database. This method encapsulates the business logic and communicates with the repository.
    
    - Injects BookRepository and UserService via constructor to persist data and retrieve user information, respectively.

--- src/main/java/com/library/api/service/BookService.java
+++ src/main/java/com/library/api/service/BookService.java
@@ -1,7 +1,31 @@
 package com.library.api.service;
 
+import com.library.api.dto.BookDTO;
+import com.library.api.model.Book;
+import com.library.api.model.ISBN;
+import com.library.api.model.User;
+import com.library.api.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+// V1.0
 
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+    private final UserService userService;
+
+    @Autowired
+    public BookService(BookRepository bookRepository, UserService userService) {
+        this.bookRepository = bookRepository;
+        this.userService = userService;
+    }
+
+    public void create(BookDTO book){
+        User author = userService.findById(book.authorId());
+        ISBN isbn = new ISBN(book.isbn());
+        Book createBook = new Book(author, book.type(), isbn, book.title());
+        bookRepository.save(createBook);
+    }
 }
