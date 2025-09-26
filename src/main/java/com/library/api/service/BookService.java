8f1a4fa0e761731aa4c14679f385c80bc43773d6
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-26 20:38:38 -0300

    feat(service): add delete method for delete books from database using id
    - refactor create method using BookMapper to map dto to book entity.

--- src/main/java/com/library/api/service/BookService.java
+++ src/main/java/com/library/api/service/BookService.java
@@ -1,14 +1,16 @@
 package com.library.api.service;
 
 import com.library.api.dto.BookDTO;
+import com.library.api.exception.BookNotFoundException;
+import com.library.api.mapper.BookMapper;
 import com.library.api.model.Book;
-import com.library.api.model.ISBN;
 import com.library.api.model.User;
 import com.library.api.repository.BookRepository;
+import com.library.api.util.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-// V1.0
+// V1.1
 
 @Service
 public class BookService {
@@ -24,8 +26,18 @@ public class BookService {
 
     public void create(BookDTO book){
         User author = userService.findById(book.authorId());
-        ISBN isbn = new ISBN(book.isbn());
-        Book createBook = new Book(author, book.type(), isbn, book.title());
+        Book createBook = BookMapper.dtoToBook(book, author);
         bookRepository.save(createBook);
+    }
+
+    public String delete(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(BookNotFoundException::new);
+        bookRepository.deleteById(id);
+        return String.format(AppConstants.BOOK_DELETED_MSG, book.getTitle());
+    }
+
+    public BookDTO findById(Long id){
+        Book book = bookRepository.findById(id).orElseThrow(BookNotFoundException::new);
+        return BookMapper.bookToDTO(book);
     }
 }
