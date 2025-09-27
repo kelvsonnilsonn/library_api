9ca3f43e0ac6b0e372eb886ab744a3c2cca37570
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-26 22:04:44 -0300

    feat(mapper): add toResponse method for map a user to BookResponseDTO

---
+++ src/main/java/com/library/api/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.library.api.mapper;
+
+import com.library.api.dto.UserRequestDTO;
+import com.library.api.dto.UserResponseDTO;
+import com.library.api.model.User;
+
+public class UserMapper {
+
+    public static User dtoToUser(UserRequestDTO dto){
+        return new User(dto.username(), dto.password());
+    }
+
+    public static UserResponseDTO toResponse(User user){
+        return new UserResponseDTO(user.getId(), user.getUsername(), user.getCreatedAt());
+    }
+}
