fb1e8e553c0e3a1aad0d33fe2eea54ae981a0c00
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-27 10:14:12 -0300

    feat(controller): implemented findByIsbn endpoint defined in BookAPI

--- src/main/java/com/library/api/controller/BookController.java
+++ src/main/java/com/library/api/controller/BookController.java
@@ -4,11 +4,10 @@ import com.library.api.dto.BookRequestDTO;
 import com.library.api.dto.BookResponseDTO;
 import com.library.api.service.BookService;
 import com.library.api.util.AppConstants;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-// V1.1
+// V1.2
 
 @RestController
 @RequestMapping(AppConstants.BOOK_BASE_PATH)
@@ -20,21 +19,27 @@ public class BookController implements BookAPI{
         this.bookService = bookService;
     }
 
-    @PostMapping(AppConstants.CREATE_BOOK_PATH)
+    @PostMapping(AppConstants.CREATE_PATH)
     public ResponseEntity<BookResponseDTO> create(@RequestBody BookRequestDTO bookRequestDTO){
         BookResponseDTO response = bookService.create(bookRequestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping
+    @DeleteMapping(AppConstants.DELETE_PATH)
     public ResponseEntity<String> delete(@RequestParam Long id){
         String message = bookService.delete(id);
         return ResponseEntity.ok(message);
     }
 
-    @Override
+    @GetMapping(AppConstants.SEARCH_PATH)
     public ResponseEntity<BookResponseDTO> findById(@RequestParam Long id) {
-        BookResponseDTO dto = bookService.findById(id);
-        return ResponseEntity.status(HttpStatus.FOUND).body(dto);
+        BookResponseDTO response = bookService.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(AppConstants.SEARCH_ISBN_PATH)
+    public ResponseEntity<BookResponseDTO> findByIsbn(@RequestParam String isbn) {
+        BookResponseDTO response = bookService.findByIsbn(isbn);
+        return ResponseEntity.ok(response);
     }
 }
