5c32dccef09f1797e7650d33527b82b0db532443
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-26 22:08:02 -0300

    feat(controller): change 'create' method return type to UserResponseDTO

--- src/main/java/com/library/api/controller/UserAPI.java
+++ src/main/java/com/library/api/controller/UserAPI.java
@@ -1,12 +1,13 @@
 package com.library.api.controller;
 
-import com.library.api.dto.UserDTO;
+import com.library.api.dto.UserRequestDTO;
+import com.library.api.dto.UserResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface UserAPI {
-    ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO);
+    ResponseEntity<UserResponseDTO> create(@RequestBody UserRequestDTO userRequestDTO);
     ResponseEntity<String> delete(@RequestParam Long id);
-    ResponseEntity<UserDTO> findById(@RequestParam Long id);
+    ResponseEntity<UserResponseDTO> findById(@RequestParam Long id);
 }
