d72d449cda5d0df94aeb91ce5783bbf01dd9f22e
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-27 10:31:46 -0300

    feat(handler): add exceptions to be handled by the controller advice

--- src/main/java/com/library/api/handler/GlobalExceptionHandler.java
+++ src/main/java/com/library/api/handler/GlobalExceptionHandler.java
@@ -1,4 +1,34 @@
 package com.library.api.handler;
 
+import com.library.api.exception.BookNotFoundException;
+import com.library.api.exception.InvalidCreatePasswordException;
+import com.library.api.exception.InvalidISBNException;
+import com.library.api.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidCreatePasswordException.class)
+    public ResponseEntity<String> handleInvalidCreatePassword(InvalidCreatePasswordException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidISBNException.class)
+    public ResponseEntity<String> handleInvalidISBN(InvalidISBNException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<String> handleBookNotFound(BookNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
 }
