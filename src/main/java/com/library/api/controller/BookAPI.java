3cc50c1e746a235588e2c07a1c063db01b59a6d4
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-27 12:36:22 -0300

    feat(controller): add swagger informations into API intefaces to describe controllers functionality

--- src/main/java/com/library/api/controller/BookAPI.java
+++ src/main/java/com/library/api/controller/BookAPI.java
@@ -2,13 +2,39 @@ package com.library.api.controller;
 
 import com.library.api.dto.BookRequestDTO;
 import com.library.api.dto.BookResponseDTO;
+import com.library.api.util.HttpConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Tag(name = "Livros", description = "Gestão de livros")
 public interface BookAPI {
+
+    @Operation(summary = "Criar um livro", description = "Criar um novo livro")
+    @ApiResponse(responseCode = HttpConstants.CREATED, description = "Livro criado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
+    @ApiResponse(responseCode = HttpConstants.CONFLICT, description = "O ISBN inserido já existe")
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<BookResponseDTO> create(@RequestBody BookRequestDTO bookRequestDTO);
+
+    @Operation(summary = "Deletar um livro", description = "Deleta um livro")
+    @ApiResponse(responseCode = HttpConstants.CREATED, description = "Livro deletado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<String> delete(@RequestParam Long id);
+
+    @Operation(summary = "Procurar um livro", description = "Procura um livro com ID")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Livro encontrado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<BookResponseDTO> findById(@RequestParam Long id);
+
+    @Operation(summary = "Procurar um livro", description = "Procurar um livro com ISBN")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Livro encontrado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<BookResponseDTO> findByIsbn(@RequestParam String isbn);
 }
--- src/main/java/com/library/api/controller/UserAPI.java
+++ src/main/java/com/library/api/controller/UserAPI.java
@@ -2,12 +2,33 @@ package com.library.api.controller;
 
 import com.library.api.dto.UserRequestDTO;
 import com.library.api.dto.UserResponseDTO;
+import com.library.api.util.HttpConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Tag(name="Usuários", description = "Gestão de usuários")
 public interface UserAPI {
+
+    @Operation(summary = "Criar um usuário", description = "Cria um novo usuário")
+    @ApiResponse(responseCode = HttpConstants.CREATED, description = "Usuário criado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
+    @ApiResponse(responseCode = HttpConstants.CONFLICT, description = "O nome de usuário inserido já existe")
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<UserResponseDTO> create(@RequestBody UserRequestDTO userRequestDTO);
+
+    @Operation(summary = "Deletar um usuário", description = "Deleta um usuário")
+    @ApiResponse(responseCode = HttpConstants.CREATED, description = "Usuário deletado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<String> delete(@RequestParam Long id);
+
+    @Operation(summary = "Procurar um usuário", description = "Procura um usuário")
+    @ApiResponse(responseCode = HttpConstants.CREATED, description = "Usuário encontrado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<UserResponseDTO> findById(@RequestParam Long id);
 }
