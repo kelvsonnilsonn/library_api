16d4d555606fca68b506c1477423bd9fe7edb13a
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-26 21:33:31 -0300

    refactor(service): create internal method to return entity directly from database
    - add findEntityById() method.
    - make findById() be used for users, then return a ResponseDTO object.

--- src/main/java/com/library/api/service/UserService.java
+++ src/main/java/com/library/api/service/UserService.java
@@ -1,28 +1,44 @@
 package com.library.api.service;
 
-import com.library.api.dto.UserDTO;
+import com.library.api.dto.UserRequestDTO;
+import com.library.api.dto.UserResponseDTO;
+import com.library.api.mapper.UserMapper;
 import com.library.api.model.User;
 import com.library.api.repository.UserRepository;
+import com.library.api.util.AppConstants;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+// V 1.2
+
 @Service
 public class UserService {
 
-    private final UserRepository userDAO;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserService(UserRepository userDAO){
-        this.userDAO = userDAO;
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
     }
 
-    public void create(UserDTO userDTO){
-        User user = new User(userDTO.username(), userDTO.password());
-        userDAO.save(user);
+    public void create(UserRequestDTO userRequestDTO){
+        userRepository.save(UserMapper.dtoToUser(userRequestDTO));
     }
 
-    public User findById(Long id){
-        return userDAO.findById(id).orElseThrow(EntityNotFoundException::new);
+    public String delete(Long id){
+        User user = findEntityById(id);
+        userRepository.deleteById(id);
+        return String.format(AppConstants.USER_DELETED_MSG, user.getUsername());
+    }
+
+
+    public UserResponseDTO findById(Long id){
+        User user = findEntityById(id);
+        return UserMapper.toResponse(user);
+    }
+
+    public User findEntityById(Long id){
+        return userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 }
