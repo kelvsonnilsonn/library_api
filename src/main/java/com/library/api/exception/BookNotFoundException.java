551d907febfe9cba0c5be094c1cc356874ba0116
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-26 22:02:15 -0300

    feat(exception): add personalized exception BookNotFoundException for not found book exception

--- src/main/java/com/library/api/exception/BookNotFoundException.java
+++ src/main/java/com/library/api/exception/BookNotFoundException.java
@@ -1,6 +1,9 @@
 package com.library.api.exception;
 
+import com.library.api.util.AppConstants;
+
 public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException() { super(AppConstants.BOOK_NOT_FOUND_MESSAGE); }
     public BookNotFoundException(String message) {
         super(message);
     }
