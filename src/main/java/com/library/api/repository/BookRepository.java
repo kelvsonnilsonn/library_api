d82db5cb5bf118a09a3f8609be26ef6d7fd12e46
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-27 10:11:43 -0300

    feat(repository): add findByIsbn method
    - use @Query jpaquery to find in database which book has the same, and unique, isbn.

--- src/main/java/com/library/api/repository/BookRepository.java
+++ src/main/java/com/library/api/repository/BookRepository.java
@@ -2,5 +2,12 @@ package com.library.api.repository;
 
 import com.library.api.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface BookRepository extends JpaRepository<Book, Long> {}
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT b FROM Book b WHERE b.isbn.number = :isbn")
+    Optional<Book> findByIsbn(String isbn);
+}
