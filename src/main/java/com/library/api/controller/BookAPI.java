d4b4008717ae205e8f0258c0b0736bcc2a9dc937
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-26 22:08:24 -0300

    feat(controller): change 'create' method return type to BookResponseDTO

--- src/main/java/com/library/api/controller/BookAPI.java
+++ src/main/java/com/library/api/controller/BookAPI.java
@@ -1,12 +1,13 @@
 package com.library.api.controller;
 
-import com.library.api.dto.BookDTO;
+import com.library.api.dto.BookRequestDTO;
+import com.library.api.dto.BookResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookAPI {
-    ResponseEntity<BookDTO> create(@RequestBody BookDTO bookDTO);
+    ResponseEntity<BookResponseDTO> create(@RequestBody BookRequestDTO bookRequestDTO);
     ResponseEntity<String> delete(@RequestParam Long id);
-    ResponseEntity<BookDTO> findById(@RequestParam Long id);
+    ResponseEntity<BookResponseDTO> findById(@RequestParam Long id);
 }
