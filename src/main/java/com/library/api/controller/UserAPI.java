2e6111d101b682d7eab47037638ab850e4529bf4
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-29 19:33:05 -0300

    refactor(controller): remove 'create' endpoint
    - auth controller already create accounts with your 'register' endpoint

--- src/main/java/com/library/api/controller/UserAPI.java
+++ src/main/java/com/library/api/controller/UserAPI.java
@@ -1,24 +1,17 @@
 package com.library.api.controller;
 
-import com.library.api.dto.UserRequestDTO;
+import com.library.api.dto.PageResponseDTO;
 import com.library.api.dto.UserResponseDTO;
 import com.library.api.util.HttpConstants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name="Usuários", description = "Gestão de usuários")
 public interface UserAPI {
-
-    @Operation(summary = "Criar um usuário", description = "Cria um novo usuário")
-    @ApiResponse(responseCode = HttpConstants.CREATED, description = "Usuário criado com sucesso")
-    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
-    @ApiResponse(responseCode = HttpConstants.CONFLICT, description = "O nome de usuário inserido já existe")
-    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
-    ResponseEntity<UserResponseDTO> create(@RequestBody UserRequestDTO userRequestDTO);
 
     @Operation(summary = "Deletar um usuário", description = "Deleta um usuário")
     @ApiResponse(responseCode = HttpConstants.CREATED, description = "Usuário deletado com sucesso")
@@ -31,4 +24,8 @@ public interface UserAPI {
     @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<UserResponseDTO> findById(@PathVariable Long id);
+
+    @Operation(summary = "Listar usuários", description = "Lista todos os usuários")
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<PageResponseDTO<UserResponseDTO>> findAll(Pageable pageable);
 }
--- src/main/java/com/library/api/controller/UserApiController.java
+++ src/main/java/com/library/api/controller/UserApiController.java
@@ -1,17 +1,15 @@
 package com.library.api.controller;
 
 import com.library.api.dto.PageResponseDTO;
-import com.library.api.dto.UserRequestDTO;
 import com.library.api.dto.UserResponseDTO;
 import com.library.api.service.UserService;
 import com.library.api.util.AppConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-// V1.3
+// V1.4
 
 @RestController
 @RequestMapping(AppConstants.USER_BASE_PATH)
@@ -19,12 +17,6 @@ import org.springframework.web.bind.annotation.*;
 public class UserApiController implements UserAPI{
 
     private final UserService userService;
-
-    @PostMapping
-    public ResponseEntity<UserResponseDTO> create(@RequestBody UserRequestDTO userRequestDTO){
-        UserResponseDTO response = userService.create(userRequestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
 
     @DeleteMapping(AppConstants.ID_PATH)
     public ResponseEntity<String> delete(@PathVariable Long id) {
