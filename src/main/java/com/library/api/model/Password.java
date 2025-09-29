e5cde9d25e3cb26868350b2cac70fec344843622
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-29 19:28:21 -0300

    fix(model): update password mode from public to private.

--- src/main/java/com/library/api/model/Password.java
+++ src/main/java/com/library/api/model/Password.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.library.api.exception.InvalidCreatePasswordException;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Password {
-    public String password;
+    private String password;
 
     @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
     public Password(String password){
