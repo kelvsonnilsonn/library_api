e51d1902c10fc3f68204a7993599f43001e6537e
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-29 19:26:58 -0300

    feat(security): add CustomUserDetailsService to load user into Spring Security system

---
+++ src/main/java/com/library/api/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.library.api.security;
+
+import com.library.api.model.User;
+import com.library.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User não encontrado"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
