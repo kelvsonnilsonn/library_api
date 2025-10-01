80cf3b8dad4f02e08f3e4a73414eeb85a8eb0b9e
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-01 12:14:44 -0300

    refactor(controller): add ApiResponses in AuthAPI methods

--- src/main/java/com/library/api/controller/AuthAPI.java
+++ src/main/java/com/library/api/controller/AuthAPI.java
@@ -2,7 +2,9 @@ package com.library.api.controller;
 
 import com.library.api.dto.auth.LoginRequestDTO;
 import com.library.api.dto.auth.RegisterRequestDTO;
+import com.library.api.util.HttpConstants;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface AuthAPI {
 
     @Operation(summary = "Logar um usuário", description = "Loga de um usuário")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Login realizado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
     ResponseEntity<?> login(@RequestBody LoginRequestDTO body);
 
     @Operation(summary = "Registrar um usuário", description = "Registra um usuário")
+    @ApiResponse(responseCode = HttpConstants.CREATED, description = "Novo usuário criado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
     ResponseEntity<?> register(@RequestBody RegisterRequestDTO body);
 
 }
