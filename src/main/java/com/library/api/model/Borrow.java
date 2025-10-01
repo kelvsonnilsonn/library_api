644f9b998b294ed48b64b1b9abc449c37c8b8e5f
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-01 10:37:30 -0300

    feat(model): add Borrow entity to represents a borrow operation
    - contain all borrow data (user, book, borrow date, due date and return date)

---
+++ src/main/java/com/library/api/model/Borrow.java
@@ -0,0 +1,45 @@
+package com.library.api.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "borrows")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Borrow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    private LocalDateTime borrowDate;
+    private LocalDateTime dueDate;
+
+    @Setter
+    private LocalDateTime returnDate;
+
+    public Borrow(User user, Book book, LocalDateTime dueDate){
+        this.user = user;
+        this.book = book;
+        this.dueDate = dueDate;
+        this.borrowDate = LocalDateTime.now();
+    }
+
+    public boolean isReturned(){
+        return returnDate != null;
+    }
+}
