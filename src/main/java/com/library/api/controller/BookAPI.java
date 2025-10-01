72b01c554e807e9f29ba637b31b2b0a5551031cd
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-01 12:15:23 -0300

    refactor(controller): add findAll method in BookAPI with documentation

--- src/main/java/com/library/api/controller/BookAPI.java
+++ src/main/java/com/library/api/controller/BookAPI.java
@@ -1,11 +1,13 @@
 package com.library.api.controller;
 
+import com.library.api.dto.PageResponseDTO;
 import com.library.api.dto.books.BookRequestDTO;
 import com.library.api.dto.books.BookResponseDTO;
 import com.library.api.util.HttpConstants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,4 +39,9 @@ public interface BookAPI {
     @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<BookResponseDTO> findByIsbn(@PathVariable String isbn);
+
+    @Operation(summary = "Listar livros", description = "Lista todos os livros")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Livros listados com sucesso")
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<PageResponseDTO<BookResponseDTO>> findAll(Pageable pageable);
 }
