b8157b2c4171293591ac2ab1bf0f59f28e72e78f
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-01 10:42:46 -0300

    feat(mapper): add BorrowMapper to map Borrows entities into BorrowResponseDTO

---
+++ src/main/java/com/library/api/mapper/BorrowMapper.java
@@ -0,0 +1,15 @@
+package com.library.api.mapper;
+
+import com.library.api.dto.borrow.BorrowResponseDTO;
+import com.library.api.model.Borrow;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface BorrowMapper {
+
+    @Mapping(target = "username", source = "user.username")
+    @Mapping(target = "bookTitle", source = "book.title")
+    @Mapping(target = "returned", expression = "java(borrow.isReturned())")
+    BorrowResponseDTO toResponse(Borrow borrow);
+}
