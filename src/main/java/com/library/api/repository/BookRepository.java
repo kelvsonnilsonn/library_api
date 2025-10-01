5ba72b4f9ec95904a3557218587cefecd953c203
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-01 15:46:17 -0300

    feat(repository) add findByTile and findByType methods in BookRepository

--- src/main/java/com/library/api/repository/BookRepository.java
+++ src/main/java/com/library/api/repository/BookRepository.java
@@ -1,6 +1,9 @@
 package com.library.api.repository;
 
+import com.library.api.enums.BookType;
 import com.library.api.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +13,10 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("SELECT b FROM Book b WHERE b.isbn.number = :isbn")
     Optional<Book> findByIsbn(String isbn);
+
+    @Query("SELECT b FROM Book b WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))")
+    Page<Book> findByTitle(Pageable pageable, String title);
+
+    @Query("SELECT b FROM Book b WHERE b.type = :type")
+    Page<Book> findByType(Pageable pageable, BookType type);
 }
