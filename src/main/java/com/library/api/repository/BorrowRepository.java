caaa03b5d9c4024912b89ddcad0ae8210502a0b1
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-01 21:11:09 -0300

    feat(repository): add findOverdueByUser method to get all user's overdue borrows

--- src/main/java/com/library/api/repository/BorrowRepository.java
+++ src/main/java/com/library/api/repository/BorrowRepository.java
@@ -6,7 +6,9 @@ import com.library.api.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface BorrowRepository extends JpaRepository<Borrow, Long> {
@@ -14,4 +16,7 @@ public interface BorrowRepository extends JpaRepository<Borrow, Long> {
     boolean existsByBookAndReturnDateIsNull(Book book);
     Optional<Borrow> findByBookIdAndUserAndReturnDateIsNull(Long bookId, User user);
     Page<Borrow> findByUserAndReturnDateIsNull(Pageable pageable, User user);
+
+    @Query("SELECT b FROM Borrow b WHERE b.user = :user AND b.returnDate IS NULL AND b.dueDate < :currentDate")
+    Page<Borrow> findOverdueByUser(Pageable pageable, User user, LocalDateTime currentDate);
 }
