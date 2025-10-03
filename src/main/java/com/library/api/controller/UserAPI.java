f73e66fb4e4dba494d212ea2c7ac270d2b3878ca
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-02 21:18:28 -0300

    feat(controller): defined updateName method to user update his name.

--- src/main/java/com/library/api/controller/UserAPI.java
+++ src/main/java/com/library/api/controller/UserAPI.java
@@ -2,6 +2,7 @@ package com.library.api.controller;
 
 import com.library.api.dto.PageResponseDTO;
 import com.library.api.dto.UserResponseDTO;
+import com.library.api.dto.UserUpdateDto;
 import com.library.api.util.HttpConstants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name="Usuários", description = "Gestão de usuários")
@@ -36,4 +38,10 @@ public interface UserAPI {
     @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<UserResponseDTO> findByUsername(@RequestParam String name);
+
+    @Operation(summary="Atualizar nome", description = "Atualiza o nome de um usuário")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Nome atualizado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<UserResponseDTO> updateName(@RequestBody UserUpdateDto updateDto);
 }
