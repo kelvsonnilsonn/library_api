a34bd2dc5d0906374fcea94287eb6bb051b4f395
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-03 19:28:31 -0300

    feat(controller): add updatePassword method to change user's password
    - injected SecurityService class instead of use business rules in controller

--- src/main/java/com/library/api/controller/AuthApiController.java
+++ src/main/java/com/library/api/controller/AuthApiController.java
@@ -1,53 +1,36 @@
 package com.library.api.controller;
 
-import com.library.api.dto.auth.LoginRequestDTO;
 import com.library.api.dto.auth.AuthResponseDTO;
+import com.library.api.dto.auth.LoginRequestDTO;
 import com.library.api.dto.auth.RegisterRequestDTO;
-import com.library.api.exception.UserNotFoundException;
-import com.library.api.model.Password;
-import com.library.api.model.User;
-import com.library.api.repository.UserRepository;
-import com.library.api.security.TokenService;
+import com.library.api.service.SecurityService;
 import com.library.api.util.AppConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(AppConstants.AUTH_BASE_PATH)
 @RequiredArgsConstructor
 public class AuthApiController implements AuthAPI {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final TokenService tokenService;
+    private final SecurityService securityService;
 
     @PostMapping(AppConstants.LOGIN_PATH)
-    public ResponseEntity<?> login(@RequestBody LoginRequestDTO body){
-        User user = this.userRepository.findByUsername(body.username()).orElseThrow(UserNotFoundException::new);
-        if(passwordEncoder.matches(body.password().getPassword(), user.getPassword())){
-            String token = this.tokenService.generateToken(user);
-            return ResponseEntity.ok(new AuthResponseDTO(token, user.getUsername()));
-        }
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO dto){
+        AuthResponseDTO response = securityService.login(dto);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping(AppConstants.REGISTER_PATH)
-    public ResponseEntity<?> register(@RequestBody RegisterRequestDTO body){
-        Optional<User> user = this.userRepository.findByUsername(body.username());
-        if(user.isEmpty()){
-            Password password = new Password(passwordEncoder.encode(body.password().getPassword()));
-            User newUser = new User(body.username(), password);
-            this.userRepository.save(newUser);
-            String token = this.tokenService.generateToken(newUser);
-            return ResponseEntity.ok(new AuthResponseDTO(token, newUser.getUsername()));
-        }
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<?> register(@RequestBody RegisterRequestDTO dto){
+        AuthResponseDTO response = securityService.register(dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping(AppConstants.CHANGE_PASSWORD_PATH)
+    public ResponseEntity<String> updatePassword(@RequestParam String password){
+        String message = securityService.update(password);
+        return ResponseEntity.ok(message);
     }
 }
