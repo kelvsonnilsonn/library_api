7f610c08b5a4121dc043f38fd698c9f69750247d
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-03 19:24:27 -0300

    refactor(model): add static modifier to 'of' and 'validate' method in Password v.o. class
    - remove validate calls from password constructor

--- src/main/java/com/library/api/model/Password.java
+++ src/main/java/com/library/api/model/Password.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Embeddable
 @Getter
@@ -15,15 +16,15 @@ public class Password {
 
     @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
     public Password(String password){
-        validate(password);
         this.password = password;
     }
 
-    public Password of(String oPassword){
-        return new Password(oPassword);
+    public static Password of(String oPassword, PasswordEncoder encoder){
+        validate(oPassword);
+        return new Password(encoder.encode(oPassword));
     }
 
-    private void validate(String pass){
+    private static void validate(String pass){
         if(pass == null || pass.length() < 3){
             throw new InvalidCreatePasswordException();
         }
