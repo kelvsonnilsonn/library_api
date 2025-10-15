b89895be67c5a0d8856a4e592b02a5bc54c3845d
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-15 16:30:42 -0300

    refactor(service): update AuthenticationInformation.java modifier from non-public to public

--- src/main/java/com/library/api/service/AuthenticationInformation.java
+++ src/main/java/com/library/api/service/AuthenticationInformation.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
-class AuthenticationInformation {
+public class AuthenticationInformation {
     public User getAuthenticatedUser(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
