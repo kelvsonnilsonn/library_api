3cf04d685b6171897588a337f3b08abb07626425
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-15 16:29:53 -0300

    refactor(mapper): update dtoToBook parameter from BookRequestDto to CreateBookCommand

--- src/main/java/com/library/api/mapper/BookMapper.java
+++ src/main/java/com/library/api/mapper/BookMapper.java
@@ -1,13 +1,14 @@
 package com.library.api.mapper;
 
+import com.library.api.command.book.CreateBookCommand;
 import com.library.api.dto.books.BookRequestDTO;
 import com.library.api.dto.books.BookResponseDTO;
 import com.library.api.model.Book;
 import com.library.api.model.User;
 
 public class BookMapper {
-    public static Book dtoToBook(BookRequestDTO bookRequestDTO, User author){
-        return new Book(author, bookRequestDTO.type(), bookRequestDTO.isbn(), bookRequestDTO.title());
+    public static Book dtoToBook(CreateBookCommand command, User author){
+        return new Book(author, command.type(), command.isbn(), command.title());
     }
 
     public static BookResponseDTO toResponse(Book book){
