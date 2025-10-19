89ca8af886b28b7bc93f101ea21dcdf203b28f87
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-19 16:50:23 -0300

    refactor(controller): remove find user endpoint. Moved it to AdminController

--- src/main/java/com/library/api/controller/UserAPI.java
+++ src/main/java/com/library/api/controller/UserAPI.java
@@ -5,18 +5,11 @@ import com.library.api.util.HttpConstants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name="Usuários", description = "Gestão de usuários")
 public interface UserAPI {
-
-    @Operation(summary = "Listar usuários", description = "Lista todos os usuários")
-    @ApiResponse(responseCode = HttpConstants.OK, description = "usuários listados com sucesso")
-    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
-    public ResponseEntity<?> findUser(Pageable pageable, @RequestParam(required=false) Long id, @RequestParam(required = false) String name);
 
     @Operation(summary="Atualizar nome", description = "Atualiza o nome de um usuário")
     @ApiResponse(responseCode = HttpConstants.OK, description = "Nome atualizado com sucesso")
--- src/main/java/com/library/api/controller/UserController.java
+++ src/main/java/com/library/api/controller/UserController.java
@@ -5,12 +5,12 @@ import com.library.api.service.command.UserCommandService;
 import com.library.api.service.query.UserQueryService;
 import com.library.api.util.AppConstants;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-// V1.5
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(AppConstants.USER_BASE_PATH)
@@ -20,17 +20,6 @@ public class UserController implements UserAPI{
 
     private final UserCommandService commandService;
     private final UserQueryService queryService;
-
-    @GetMapping
-    public ResponseEntity<?> findUser(Pageable pageable, @RequestParam(required=false) Long id, @RequestParam(required = false) String name){
-        if(id != null){
-            return ResponseEntity.ok(queryService.findById(id));
-        }
-        if(name != null){
-            return ResponseEntity.ok(queryService.findByUsername(name));
-        }
-        return ResponseEntity.ok(queryService.findAll(pageable));
-    }
 
     @PutMapping
     public ResponseEntity<Void> updateName(@RequestBody UpdateUsernameCommand command){
